4356dffc11f53dc57ae9bd4e7fd4683aac3175c2
amazingsora <lgaz830304@gmail.com>
2020-07-04 08:18:44 +0800

    第2.1次上傳

---
+++ src/main/java/Leetcode/A002/Add_Tw_Numbers.java
@@ -0,0 +1,18 @@
+package Leetcode.A002;
+
+//You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//        　　Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//        　　Output: 7 -> 0 -> 8
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+
+
+
+public class Add_Tw_Numbers {
+    public static void main(String[] args) {
+    }
+     public ListNode list(int mun1[],int mun2[]){
+
+
+    }
+}
